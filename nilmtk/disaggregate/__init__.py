78b81fb8bb7f4170c8166c3bb2aa6d535b495727
Maximilian Wurm <Maximilian Wurm>
2017-08-02 10:59:40 +0200

    Added the init file to include the new disaggregator. Was missing in previous commit.

--- nilmtk/disaggregate/__init__.py
+++ nilmtk/disaggregate/__init__.py
@@ -3,4 +3,5 @@ from .combinatorial_optimisation import CombinatorialOptimisation
 from .fhmm_exact import FHMM
 from .hart_85 import Hart85
 from .maximum_likelihood_estimation import MLE
+from .custom_baranski import CustomBaranski
 
