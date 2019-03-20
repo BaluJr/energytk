d56292e9abe802cec407f9a354930672ec4e05d5
Maximilian Wurm <maximilian.wurm@gmx.net>
2019-03-20 19:19:23 +0100

    Adding the VEP-connector to the public repositiory as a showcase for connecting the Rest API.

--- nilmtk/datastore/vepconnector.py
+++ nilmtk/datastore/vepconnector.py
@@ -12,6 +12,7 @@ from builtins import range
 import json
 import logging
 from nilmtk.measurement import LEVEL_NAMES
+import datetime
 
 # do not edit! added by PythonBreakpoints
 from pdb import set_trace as _breakpoint
@@ -20,13 +21,13 @@ from pdb import set_trace as _breakpoint
 class VEPConnector(DataStore):
     ''' 
     This DataStore sets up a connection to VEP and handles all 
-    data over the REST API.
+    data movement over the REST API.
 
-    For VEP the buildings are the different grids.
+    For VEP, the buildings of EnergyTK are the different grids.
     '''
 
     @doc_inherit
-    def __init__(self, api_address, datastore_kwargs = None, logging = False):
+    def __init__(self, api_address, datastore_kwargs = None, logging = True):
         '''
         Sets up the connector to the VEP plattform.
         
@@ -62,7 +63,9 @@ class VEPConnector(DataStore):
         #Get Token
         data = "grant_type=password&username=" + self.username +"&password=" + self.password
         r = requests.post(self.base_url + "gettoken", data = data)
-        self.auth_token = json.loads(r.text)["access_token"]
+        tst = r.text
+        tst = json.loads(tst)
+        self.auth_token = tst["access_token"]
         
         self.metadata = None
         super(VEPConnector, self).__init__()
@@ -70,8 +73,8 @@ class VEPConnector(DataStore):
 
     def _check_connections(self):
         '''
-        Check if the connection is working.
-        Has to be moved into tests later on.
+        Checks whether the connection is working.
+        Can be moved to the tests later on.
         '''
        
         if logging:
@@ -92,7 +95,7 @@ class VEPConnector(DataStore):
         test = json.loads(r.text)
 
 
-    def _execute_request(self, fn, parameters = {}, type = "GET", body_data = {}):
+    def _execute_request(self, fn, parameters = {}, type = "GET", body_data = {}, files = {}):
         '''
         This is the internal helper function to send a request to the VEP API.
         
@@ -109,21 +112,26 @@ class VEPConnector(DataStore):
         body_data:
             Only used for POST requests. The payload sent within
             the body of the message.
+        files:
+            When there are additional files which have to be sent to the server.
 
         Returns
         -------
         The result of the server request.
         '''
 
-        header = {"Authorization": "Bearer " + self.auth_token}
+        header = {"Authorization": "Bearer " + self.auth_token, "Content-type": "application/json"}
         target_url = self.base_url + "/EnergyTK/" + fn + "/" + "/".join(parameters.values())
 
         if type == "GET":
             r = requests.get(target_url, headers = header)
         else:
-            r = requests.post(target_url, headers = header, data = body_data)
+            r = requests.post(target_url, headers = header, json = body_data)
 
-        result = json.loads(r.text)
+        result = None
+        txt = r.text 
+        if txt != "":
+            result = json.loads(txt)
         return result
 
     
@@ -133,7 +141,6 @@ class VEPConnector(DataStore):
         testing on the serverside.
         '''
         self._execute_request("test")
-        pass
         return
 
 
@@ -185,8 +192,8 @@ class VEPConnector(DataStore):
         A pandas dataframe with indices as used by EnergyTk.
         '''
         timestamps = data["TimeStamps"]
+        data = data["Values"]
         idx = pd.DatetimeIndex(timestamps, tz = "UTC")
-        data = np.transpose(data["Values"])
         columns = pd.MultiIndex.from_tuples(columns, names = LEVEL_NAMES)
         df = pd.DataFrame(data=data, index=idx, columns = columns)
         return df
@@ -197,6 +204,8 @@ class VEPConnector(DataStore):
              chunksize=MAX_MEM_ALLOWANCE_IN_BYTES, verbose=False, **additionalLoaderKwargs):
         '''
         Load measurments over a certain period of time.
+        The resampling is taking place on the serverside to save bandwidth. This is different from the 
+        HDF datastore where always all data is loaded and then resampled.
         '''
         # TODO: calculate chunksize default based on physical
         # memory installed and number of columns
@@ -206,6 +215,8 @@ class VEPConnector(DataStore):
             key = '/' + key
         if len(key) > 1 and key[-1] == '/':
             key = key[:-1]
+
+        sample_period = additionalLoaderKwargs["sample_period"]
 
         # Make sure chunksize is an int otherwise `range` complains later.
         chunksize = np.int64(chunksize)
@@ -245,29 +256,27 @@ class VEPConnector(DataStore):
 
             # The estimation of fitting slices is avoided
             delta = section.end -  section.start
-            n_chunks = int(np.ceil((delta.total_seconds() / (15*60) / chunksize)))
+            n_chunks = int(np.ceil((delta.total_seconds() / sample_period / chunksize)))
             delta = delta/n_chunks
-
             slice_starts = []
             for i in range(n_chunks):
                 slice_starts.append(section.start + delta * i)
-            #n_chunks = int(np.ceil((section.end - section.start) / chunksize))
-            #if n_chunks > 1:
-                #self.all_sections_smaller_than_chunksize = False
+            if n_chunks > 1:
+                self.all_sections_smaller_than_chunksize = False
 
+            # Load the sections
             for chunk_i, chunk_start_i in enumerate(slice_starts):
-                chunk_end_i = chunk_start_i + chunksize * pd.Timedelta('15 minutes')
+                chunk_end_i = chunk_start_i + datetime.timedelta(seconds=int(chunksize))
                 there_are_more_subchunks = (chunk_i < n_chunks-1)
 
                 if chunk_end_i > section.end:
                     chunk_end_i = section.end
 
-                # The required parameter form is: base={lat}/{lng}/{deviceKey}/{deviceType} + {start}/{end}/{columns}
+                # The required parameter form is: base={lat}/{lng}/{deviceKey}/{deviceType} + {start}/{end}/{columns}/{sample_rate}
                 iso_chunk_start = chunk_start_i.isoformat()
                 iso_chunk_end = chunk_end_i.isoformat()
-                data = self._execute_request("load", type = "GET", parameters = {"url": key, "start": iso_chunk_start, "end": iso_chunk_end, "columns": columnsStr}) 
+                data = self._execute_request("load", type = "GET", parameters = {"url": key, "start": iso_chunk_start, "end": iso_chunk_end, "columns": columnsStr, "sample_period": str(sample_period)}) 
                 data = self._jsonDataToPandasDF(columns, data)
-                #data = self.store.select(key=key, columns=cols_idx, start=chunk_start_i, stop=chunk_end_i)
 
                 if len(data) <= 2:
                     data = pd.DataFrame(columns=cols_idx)
@@ -280,10 +289,12 @@ class VEPConnector(DataStore):
                         look_ahead_start_i = chunk_end_i
                         look_ahead_end_i = look_ahead_start_i + n_look_ahead_rows
                         try:
-                            data.look_ahead = self.store.select(
-                                key=key, columns=columns,
-                                start=look_ahead_start_i,
-                                stop=look_ahead_end_i)
+                            #data.look_ahead = self.store.select(
+                            #    key=key, columns=columns,
+                            #    start=look_ahead_start_i,
+                            #    stop=look_ahead_end_i)
+                            data = self._execute_request("load", type = "GET", parameters = {"url": key, "start": look_ahead_start_i, "end": look_ahead_end_i, "columns": columnsStr, "sample_period": sample_period}) 
+                            data = self._jsonDataToPandasDF(columns, data)
                         except ValueError:
                             data.look_ahead = pd.DataFrame()
                     else:
@@ -298,32 +309,36 @@ class VEPConnector(DataStore):
     @doc_inherit
     def append(self, key, value):
         """
+        Note: The sending into this direction is using the json format of pandas.
+        There is not too much data to send.
+
         Parameters
         ----------
         key : str
+            The key of the timeline to store.
         value : pd.DataFrame
-
+            The dataframe containin the timestamps and the different 
+            values.                             
         Notes
         -----
-        To quote the Pandas documentation for pandas.io.pytables.HDFStore.append:
-        Append does *not* check if data being appended overlaps with existing
-        data in the table, so be careful.
+        
         """
+        value.to_json()
         data = self._execute_request("load", type = "POST", parameters = {"id": key}, body_data = {"value": value}) 
 
 
     @doc_inherit
-    def put(self, key, value, fixed = False):
+    def put(self, key, values, fixed = False):
         """
         Stores a new timeline inside the VEP system.
 
         Parameters
         ----------
         key : str
-        value : pd.DataFrame
+        values : pd.Series
         """
-        
-        data = self._execute_request("put", type = "POST", parameters = {"id": key}, body_data = {"value": value}) 
+        values = values.to_json(date_format='iso', double_precision=3) #, orient='split')
+        data = self._execute_request("put", type = "POST", parameters = {"id": key}, body_data = values ) 
 
     @doc_inherit
     def remove(self, key):
@@ -334,7 +349,7 @@ class VEPConnector(DataStore):
         ----------
         key : str
         """
-        raise NotImplementedError("Since the VEP Api has been used for reading data only. Remove data has not been implemented yet.")
+        raise NotImplementedError("Since the EnergyTk is currently used in a readonly fashion, remove data has not been implemented yet.")
     
 
     @doc_inherit
@@ -371,12 +386,12 @@ class VEPConnector(DataStore):
         ----------
         key : str
         """
-        raise NotImplementedError("Since the VEP Api has been used for reading data only it is not supported to store metadata.")
+        raise NotImplementedError("Since the EnergyTk is currently used in a readonly fashion, remove data has not been implemented yet.")
     
 
     @doc_inherit
     def update_root_metadata(self, new_metadata):
-        raise NotImplementedError("Since the VEP Api has been used for reading data only it is not supported to store metadata.")
+        raise NotImplementedError("Since the EnergyTk is currently used in a readonly fashion, remove data has not been implemented yet.")
 
     @doc_inherit
     def elements_below_key(self, key='/'):
@@ -425,43 +440,40 @@ class VEPConnector(DataStore):
         return timeframe
         
 
-    def store_model(self, key, row_values, blob):
+    def store_model(self, model):
         """
         The VEP API stores the row values within easily accessible table rows.
         The blob is stored at a separate location. 
 
+        It is a little bit differen than the other calls as a mixture of files and 
+        normal attributes has to be sent. 
 
         Parameters
         ----------
-        key : string, optional
-            The key of the measurment device
-        row_values: string
-            Serialized object in readable form. This part can be later 
-            read without deserializing the blob.
-        blob: string
-            A binary object which is stored as a whole. 
+        model : object
+            The model which is sent to the server
         """
-        metadata = self._execute_request("store_model", type = "POST", parameters = {"device": key}) 
+
+        self._execute_request("store_model", type = "POST", body_data = model)
 
     
-    def get_model(self, key):
+    def get_model(self, modelId):
         """
         This function returns a model stored in the storage.
 
 
         Parameters
         ----------
-        key : string, optional
-            The key of the measurment device
-        row_values: string
-            Serialized object in readable form. This part can be later 
-            read without deserializing the blob.
-        blob: string
-            A binary object which is stored as a whole. 
+        modelId : uuid as string 
+            The uuid of the model to load.
         """
-        metadata = self._execute_request("get_model", type = "GET", parameters = {"device": key}) 
-
-
+        model = self._execute_request("get_model", type = "GET", parameters = {"modelId": modelId})
+        
+        # Reconstruct lists from strings on server 
+        for prop in model:
+            if isinstance(model[prop], str) and model[prop][0] == "[":
+                exec("model[prop] = " + model[prop])
+        return model
 
 
 def _timeframe_for_chunk(there_are_more_subchunks, chunk_i, window_intersect, index):
